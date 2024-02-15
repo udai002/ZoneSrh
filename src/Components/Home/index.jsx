85eafee5540c9981343859a0aed9959c1de52dd9
udai sai <karmuriudaisai002@gmail.com>
2024-02-15 23:46:12 +0530

    add framer motion

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,8 +2,15 @@ import { Component } from "react";
 
 class Home extends Component{
     render(){
-        return <div className="pt-16 bg-[url('lawgate.jpg')] h-[100vh] bg-no-repeat text-white">
-            this is a Home Component
+        return <div className="md:pt-16  h-[70vh] bg-amber-100">
+            <div className="p-4 flex flex-col items-center ">
+            <h1 className="text-4xl font-bold text-center pt-12 my-5">ZoneSRH Makes together</h1>
+            <p className="text-2xl text-center">confused where to stay?.. Here you are find your roommate and build memories</p>
+            <button className="px-4 py-2 bg-black text-white rounded mt-5 ">Explore</button>
+            </div>
+            <div>
+
+            </div>
         </div>
     }
 }
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,52 @@
-const Navbar = ()=><div className="w-full fixed top-0">
-        <div className="p-4 flex sticky inset-0 z-50  bg-slate-500/20 text-white bg-opacity-45">
-            <h1 className="text-2xl font-bold mx-6">ZoneSrh</h1>
-            <ul className="flex flex-row text-lg mt-1">
+import { useState } from "react"
+import { IoCloseCircle } from "react-icons/io5";
+import {motion} from 'framer-motion'
+
+const Navbar = ()=>{ 
+    const [open , setOpen] = useState(false)
+    
+    return <div className="w-full flex flex-row justify-between sticky inset-0 z-50   bg-opacity-45 ">
+        <div className=" flex ">
+            <h1 className="text-2xl font-bold mx-6 pt-4">ZoneSrh</h1>
+            <ul className="flex-row text-lg mt-1 hidden md:flex pt-4">
                 <li className="mx-4">Home</li>
                 <li className="mx-4">Location</li>
                 <li className="mx-4">About</li>
+                <li className="mx-4">Register</li>
             </ul>
         </div>
-</div>
+        <div className="p-4 hidden md:block">
+        <button className="mr-4 btn-black">Signin</button>
+        <button className='btn-black'>Signup</button>
+        </div>
 
+
+{/* this is a opening menu  */}
+        <div className="p-4 md:hidden" onClick={()=>{setOpen(true)}}>
+            <div className="w-8 border-b-[2px] border-black"></div>
+            <div className="w-8 border-b-[2px] pt-2 border-black"></div>
+            <div className="w-8 border-b-[2px] pt-2 border-black"></div>
+        </div>
+
+
+        {/* this is sideNavbar */}
+        {open?<motion.div initial={{x:-250}} animate={{x:0}} className="fixed left-0 w-[240px] bg-white h-[100vh] flex-col flex justify-between transition-transform transform translate-x-400 ease-in-out duration-300">
+            <div className="flex flex-row justify-end fixed right-1 " onClick={()=>{setOpen(false)}}>
+            <IoCloseCircle className="text-3xl m-4" />
+            </div>
+            <ul className="p-4 text-center text-xl flex flex-col pt-10">
+                <li className="mt-3">Home</li>
+                <li className="mt-3">Location</li>
+                <li className="mt-3">Register</li>
+                <li className="mt-3">About</li>
+            </ul>
+            <div className="p-4 ">
+        <button className="mx-4 btn-black">Signin</button>
+        <button className='btn-black'>Signup</button>
+        </div>
+        </motion.div>:''}
+        
+</div>
+}
 
 export default Navbar 
