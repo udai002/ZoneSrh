214c9ac27b5db344232917e01bc8007ba98a3055
udai sai <karmuriudaisai002@gmail.com>
2024-02-15 23:44:40 +0530

    first commit

---
+++ src/Components/Home/index.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Home extends Component{
+    render(){
+        return <div className="pt-16 bg-[url('lawgate.jpg')] h-[100vh] bg-no-repeat text-white">
+            this is a Home Component
+        </div>
+    }
+}
+
+
+export default Home
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+const Navbar = ()=><div className="w-full fixed top-0">
+        <div className="p-4 flex sticky inset-0 z-50  bg-slate-500/20 text-white bg-opacity-45">
+            <h1 className="text-2xl font-bold mx-6">ZoneSrh</h1>
+            <ul className="flex flex-row text-lg mt-1">
+                <li className="mx-4">Home</li>
+                <li className="mx-4">Location</li>
+                <li className="mx-4">About</li>
+            </ul>
+        </div>
+</div>
+
+
+export default Navbar 
